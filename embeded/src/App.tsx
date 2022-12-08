2e0efec1307aae384cca9e4d7ef393472c24b676
Alejandro Nanez <git@alejandro.dev>
2022-12-07 20:39:42 -0500

    refactor: clean up embedded application

--- embeded/src/App.tsx
+++ embeded/src/App.tsx
@@ -1,27 +1,27 @@
 import { useCallback, useEffect, useState } from "react";
 import "./App.css";
 
-const SUCCESS_EVENT = "merchant:event:success";
-const CANCEL_EVENT = "merchant:event:cancel";
-const URL = "http://127.0.0.1:5174/";
 const PARENT_URL = "http://127.0.0.1:5173/";
 
 function App() {
   const [messageFromParent, setMessageFromParent] = useState("");
 
-  const callback = useCallback((e: any) => {
-    console.log("child");
+  const receiveMessageCallback = useCallback((e: MessageEvent) => {
     setMessageFromParent(e.data);
   }, []);
 
   useEffect(() => {
-    window.addEventListener("message", callback);
+    window.addEventListener("message", receiveMessageCallback);
 
-    return () => window.removeEventListener("message", callback);
+    return () => window.removeEventListener("message", receiveMessageCallback);
   }, []);
 
   const handleClickSuccess = () => {
-    window.parent.postMessage("Hi dad!", PARENT_URL);
+    window.parent.postMessage("'Success' action", PARENT_URL);
+  };
+
+  const handleClickCancel = () => {
+    window.parent.postMessage("'Cancel' action", PARENT_URL);
   };
 
   return (
@@ -37,7 +37,7 @@ function App() {
       <button onClick={handleClickSuccess}>Success</button>
       <br />
       <br />
-      <button>Cancel</button>
+      <button onClick={handleClickCancel}>Cancel</button>
     </div>
   );
 }
