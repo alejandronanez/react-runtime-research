76d36a99603e17bcf9a788ecb60b174fe76525f9
Alejandro Nanez <git@alejandro.dev>
2022-12-07 21:11:01 -0500

    refactor: created an Embedded component to clean things up at the App level

--- iframe/src/App.tsx
+++ iframe/src/App.tsx
@@ -1,58 +1,31 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
 import './App.css';
+import { Embedded } from './Embedded';
 
 type AppProps = {
   embeddedUrl: string;
   embeddedSuccessEvent: string;
   embeddedCancelEvent: string;
 };
+
 function App({
   embeddedCancelEvent,
   embeddedSuccessEvent,
   embeddedUrl,
 }: AppProps) {
-  const [messageFromChild, setMessageFromChild] = useState('');
-  const iframeRef = useRef<HTMLIFrameElement>(null);
-  const sendMessage = () => {
-    if (!iframeRef?.current?.contentWindow) {
-      return;
-    }
-
-    iframeRef.current.contentWindow.postMessage(
-      'message sent from the parent',
-      embeddedUrl,
-    );
-  };
-
-  const receiveMessageCallback = useCallback((e: MessageEvent) => {
-    /**
-     * e.data could be an object with information from React. We can't update the state with an object
-     *
-     * Objects look like:
-     * https://cdn.zappy.app/7f3ffc8c626d9c2b04d64a7f4048027b.png
-     */
-    if (typeof e.data === 'string') {
-      setMessageFromChild(e.data);
-    }
-  }, []);
-
-  useEffect(() => {
-    window.addEventListener('message', receiveMessageCallback);
-
-    return () => window.removeEventListener('message', receiveMessageCallback);
-  }, []);
-
   return (
     <div className="App">
       <h1>RechargeApps</h1>
-      <div className="card">
-        <button onClick={sendMessage}>Message to child</button>
-      </div>
-      <pre>Navigate to: {messageFromChild}</pre>
-      <iframe
-        ref={iframeRef}
-        style={{ height: 600, width: 600, border: 'none' }}
-        src={embeddedUrl}
+
+      <Embedded
+        embeddedCancelEvent={embeddedCancelEvent}
+        embeddedSuccessEvent={embeddedSuccessEvent}
+        embeddedUrl={embeddedUrl}
+        onSuccessfulEvent={(event: string) => {
+          console.log('Successful Event: ', event);
+        }}
+        onCancelEvent={(event: string) => {
+          console.log('Cancel Event: ', event);
+        }}
       />
     </div>
   );
---
+++ iframe/src/Embedded.tsx
@@ -0,0 +1,69 @@
+import { useCallback, useEffect, useRef } from 'react';
+
+type EmbeddedProps = {
+  embeddedUrl: string;
+  embeddedCancelEvent: string;
+  embeddedSuccessEvent: string;
+  onSuccessfulEvent: (event: string) => void;
+  onCancelEvent: (event: string) => void;
+};
+
+export const Embedded = ({
+  embeddedCancelEvent,
+  embeddedSuccessEvent,
+  embeddedUrl,
+  onCancelEvent,
+  onSuccessfulEvent,
+}: EmbeddedProps) => {
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+
+  const receiveMessageCallback = useCallback((e: MessageEvent) => {
+    /**
+     * e.data could be an object with information from React. We can't update the state with an object
+     *
+     * Objects look like:
+     * https://cdn.zappy.app/7f3ffc8c626d9c2b04d64a7f4048027b.png
+     */
+    if (typeof e.data === 'string') {
+      onCancelEvent(embeddedCancelEvent);
+      onSuccessfulEvent(embeddedSuccessEvent);
+    }
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('message', receiveMessageCallback);
+
+    return () => window.removeEventListener('message', receiveMessageCallback);
+  }, []);
+
+  const sendMessageToIframe = () => {
+    if (!iframeRef?.current?.contentWindow) {
+      return;
+    }
+
+    iframeRef.current.contentWindow.postMessage(
+      'message sent from the parent',
+      embeddedUrl,
+    );
+  };
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '10px',
+        marginTop: 20,
+      }}
+    >
+      <button onClick={sendMessageToIframe}>
+        Send a message to the iFrame
+      </button>
+      <iframe
+        ref={iframeRef}
+        style={{ height: 600, width: 600, border: 'none' }}
+        src={embeddedUrl}
+      />
+    </div>
+  );
+};
